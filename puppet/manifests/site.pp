4fcb4acc6f39022eab07735bc5ad9d7cbeff1a4d
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-07-18 15:09:54 +0100

    try and setup initial configurations in a sequential way

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -5,27 +5,32 @@ import 'functions.pp'
 # Import node definitions from any additional manifests
 import 'nodes/*.pp'
 
-# Apply s_basenode to clients that lack more specific configuration
+# Default thing to do for all nodes
 node "default" {
-#  include s_basenode
-        file { "/etc/profile.d/proxy.sh": ensure => present, }
+	file { "/etc/profile.d/proxy.sh": ensure => present, }
 
-        line { http_proxy:
-                file => "/etc/profile.d/proxy.sh",
-                line => "export http_proxy=http://proxy.tchpc.tcd.ie:8080",
-        }
+	line { http_proxy:
+		file => "/etc/profile.d/proxy.sh",
+		line => "export http_proxy=http://proxy.tchpc.tcd.ie:8080",
+		require => File["/etc/profile.d/proxy.sh"],
+	}
 
-        line { https_proxy:
-                file => "/etc/profile.d/proxy.sh",
-                line => "export https_proxy=http://proxy.tchpc.tcd.ie:8080",
-        }
+	line { https_proxy:
+		file => "/etc/profile.d/proxy.sh",
+		line => "export https_proxy=http://proxy.tchpc.tcd.ie:8080",
+		require => File["/etc/profile.d/proxy.sh"],
+	}
 
-        line { ftp_proxy:
-                file => "/etc/profile.d/proxy.sh",
-                line => "export ftp_proxy=http://proxy.tchpc.tcd.ie:8080",
-        }
+	line { ftp_proxy:
+		file => "/etc/profile.d/proxy.sh",
+		line => "export ftp_proxy=http://proxy.tchpc.tcd.ie:8080",
+		require => File["/etc/profile.d/proxy.sh"],
+	}
+
 
         Package { ensure => "installed" }
         $enhancers = [ "screen", "strace", "sudo", "git", "gcc-gfortran" ]
-        package { $enhancers: }
+        package { $enhancers:
+		require => Line["http_proxy", "https_proxy", "ftp_proxy"],
+	}
 }
