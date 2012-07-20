ed1739167443673cee27477525d12542a93a12d9
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-07-20 13:26:27 +0100

    get clever with a tchpc only module so we can include just this configuration for tchpc site

--- puppet/modules/tchpc/manifests/init.pp
+++ puppet/modules/tchpc/manifests/init.pp
@@ -37,7 +37,7 @@
 #
 class tchpc {
 
-	case $operatingsystem {
+    case $operatingsystem {
         'scientific', 'redhat', 'centos': {
             $supported = true
         }
@@ -49,4 +49,41 @@ class tchpc {
         }
     }
 
+    if ( $supported == true ) {
+	class { "tchpc::proxy": }
+    }
+
+}
+
+class tchpc::proxy {
+	file { "/etc/profile.d/proxy.sh": ensure => present, }
+
+	line { http_proxy:
+		file => "/etc/profile.d/proxy.sh",
+		line => "export http_proxy=http://proxy.tchpc.tcd.ie:8080",
+		require => File["/etc/profile.d/proxy.sh"],
+	}
+
+	line { https_proxy:
+		file => "/etc/profile.d/proxy.sh",
+		line => "export https_proxy=http://proxy.tchpc.tcd.ie:8080",
+		require => File["/etc/profile.d/proxy.sh"],
+	}
+
+	line { ftp_proxy:
+		file => "/etc/profile.d/proxy.sh",
+		line => "export ftp_proxy=http://proxy.tchpc.tcd.ie:8080",
+		require => File["/etc/profile.d/proxy.sh"],
+	}
+
+	line { http_proxy_yum:
+		file => "/etc/yum.conf",
+		line => "proxy=http://proxy.tchpc.tcd.ie:8080",
+	}
+
+        Package { ensure => "installed" }
+        $enhancers = [ "screen", "strace", "sudo", "git" ]
+        package { $enhancers:
+               require => Line["http_proxy_yum"],
+	}
 }
