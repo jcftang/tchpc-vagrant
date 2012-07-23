4d4a1d187a50550aee40ba30b8f930663e7cef6a
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-07-23 14:27:44 +0100

    sample for installing rvm, ruby and some gems, doesn't seem to work properly as rvm overrides GEM_PATH which breaks puppet for subsequent runs

--- puppet/manifests/nodes/web.pp
+++ puppet/manifests/nodes/web.pp
@@ -10,4 +10,24 @@ node 'web.localhost' inherits default {
 		ensure => "running",
 		require => Package["httpd"],
 	}
+
+#	include rvm
+#	rvm::system_user { vagrant: ; }
+#	rvm_system_ruby {
+#		'ruby-1.9.3-p194':
+#		ensure => 'present',
+#		default_use => false;
+#	}
+#
+#	rvm_gemset {
+#		"ruby-1.9.3-p194@vagrant":
+#		ensure => 'present',
+#		require => Rvm_system_ruby['ruby-1.9.3-p194'];
+#	}
+#
+#	rvm_gem {
+#		'ruby-1.9.3-p194@vagrant/bundler':
+#		ensure => 'present',
+#		require => Rvm_gemset['ruby-1.9.3-p194@vagrant'];
+#	}
 }
