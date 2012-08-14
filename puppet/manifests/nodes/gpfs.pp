3a2ca82071eafc3cc2661af23f99cb0833210828
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-08-14 17:08:53 +0100

    first attempt at integrating gpfs
    Conflicts:
    	Vagrantfile

---
+++ puppet/manifests/nodes/gpfs.pp
@@ -0,0 +1,16 @@
+#
+# template for tchpc site, there are dependancy issues with proxies
+#
+node /gpfs.*\.localhost/ inherits default {
+	# install needed packages
+	Package { ensure => "installed" }
+	$enhancers = [ "httpd", "rpm-build", "gcc", "gcc-c++", "kernel-devel", "nfs-utils" ]
+	package { $enhancers: }
+
+	# turn on services
+	service { "httpd":
+		enable => true,
+		ensure => "running",
+		require => Package["httpd"],
+	}
+}
