baf38da227a90b2636e9ba9100966e52c5db64c8
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-08-22 20:50:53 +0100

    Turn on avahi for mdns resolution for GPFS
    This can be generalised and reused elsewhere

--- puppet/manifests/nodes/gpfs.pp
+++ puppet/manifests/nodes/gpfs.pp
@@ -6,14 +6,36 @@
 #
 node /gpfs.*\.localhost/ inherits default {
 	# install needed packages
-	Package { ensure => "installed" }
-	$enhancers = [ "httpd", "rpm-build", "gcc", "gcc-c++", "kernel-devel", "nfs-utils" ]
-	package { $enhancers: }
+	# generic devel tools and libraries
+	if ! defined(Package['rpm-build']) { package { 'rpm-build': ensure => installed } }
+	if ! defined(Package['kernel-devel']) { package { 'kernel-devel': ensure => installed } }
+	if ! defined(Package['gcc']) { package { 'gcc': ensure => installed } }
+	if ! defined(Package['gcc-c++']) { package { 'gcc-c++': ensure => installed } }
+	if ! defined(Package['nfs-utils']) { package { 'nfs-utils': ensure => installed } }
+
+	# bonjour/zeroconf/avahi adhoc dns entries
+	if ! defined(Package['yum-conf-epel']) { package { 'yum-conf-epel': ensure => installed } }
+	if ! defined(Package['nss-mdns']) { package { 'nss-mdns': ensure => installed,
+							require => Package['yum-conf-epel'] } }
+	if ! defined(Package['avahi-tools']) { package { 'avahi-tools': ensure => installed } }
+	if ! defined(Package['avahi-dnsconfd']) { package { 'avahi-dnsconfd': ensure => installed } }
+	if ! defined(Package['avahi']) { package { 'avahi': ensure => installed } }
 
 	# turn on services
-	service { "httpd":
+	service { "avahi-daemon":
 		enable => true,
 		ensure => "running",
-		require => Package["httpd"],
+		require => Package["avahi"],
 	}
+	service { "avahi-dnsconfd":
+		enable => true,
+		ensure => "running",
+		require => Package["avahi-dnsconfd"],
+	}
+
+#	service { "httpd":
+#		enable => true,
+#		ensure => "running",
+#		require => Package["httpd"],
+#	}
 }
